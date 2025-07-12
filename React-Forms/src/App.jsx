ebfafc8ca8fda99cc3200b96d579b142f0163305
mynk-13 <mynk13.projects@gmail.com>
2025-07-13 01:40:07 +0530

    Added React Form Submission- using state variables (seperate)

---
+++ React-Forms/src/App.jsx
@@ -0,0 +1,16 @@
+import StateLogin from "./StateLogin"
+
+
+
+function App() {
+  
+
+  return (
+    <>
+    <h2>Forms</h2>
+      <StateLogin />
+    </>
+  )
+}
+
+export default App
---
+++ React-Forms/src/StateLogin.jsx
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+import "./StateLogin.css";
+
+const StateLogin = () => {
+
+    const [emailValue, setEmailValue]=useState("");
+    const [passwordValue, setPasswordValue]=useState("");
+
+    const handleSubmit=(e)=>{
+        e.preventDefault();
+        console.log("Form Submitted !!!");
+
+        console.log("Email : " , emailValue);
+        console.log("Password : ", passwordValue);
+    }
+
+  return (
+    <div>
+        <form onSubmit={handleSubmit}>
+         <h2>StateLogin</h2>
+        <div className='form-control'>
+        <label htmlFor='email' className='form-label'>Email</label><br />
+        <input id="email" type="email" name='email' value={emailValue} onChange={(e)=> setEmailValue(e.target.value)} />  
+        </div>
+
+        <div className='form-control'>
+        <label htmlFor='password' className='form-label'>Password</label><br />
+        <input id="password" type="password" name='password' value={passwordValue} onChange={(e)=> setPasswordValue(e.target.value)} />  
+        </div>
+
+        <div className='btn-container'>
+        <button className='bt-form' type="reset">Reset</button>
+        <button className='btn-form'>Login</button>
+        </div>
+
+        </form>
+           
+    </div>
+  )
+}
+
+export default StateLogin
